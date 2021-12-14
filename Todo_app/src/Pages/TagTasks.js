e9272e9fc19082d4ba0c424f256c42c2f84e4a0f
Ayush Kumar <2kayush.125@gmail.com>
2021-12-14 18:29:02 +0530

    todo_app

--- Todo_app/src/Pages/TagTasks.js
+++ Todo_app/src/Pages/TagTasks.js
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getTodos, changeId } from "../store/allTasks";
 
 const TagTasks = () => {
+  let [value, setValue] = useState(1);
+  // let [id, setId] = useState(1);
+  const dispatch = useDispatch();
+  let { todos, id } = useSelector((state) => state);
+  useEffect(() => {
+    dispatch(getTodos(id));
+  }, [dispatch, id]);
+  // console.log(todos);
+  let ctr = 1;
   return (
-    <div>
-      <h1>Tasks by tags</h1>
+    <div className="container">
+      <form className="d-flex my-3 mx-3" style={{ width: "25%" }}>
+        <input
+          className="form-control me-2"
+          type="number"
+          placeholder="Search tasks by tag Name"
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <button
+          className="btn btn-outline-success my-2 my-sm-0"
+          onClick={() => {
+            // console.log("Clicked search task");
+            // dispatch(getTodos(value));
+            // setId(value);
+            id = value;
+            changeId(value);
+          }}
+        >
+          Search
+        </button>
+      </form>
+      <h1>User {id}'s Tasks: </h1>
+      {todos.map((todo) => {
+        return (
+          <div key={ctr}>
+            <h3>
+              {ctr++}: {todo.title}
+            </h3>
+            <p>{todo.completed}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- Todo_app/src/store/allTasks.js
+++ Todo_app/src/store/allTasks.js
@@ -1,10 +1,39 @@
 const GET_TODOS = "GET_TODOS";
+const CHNG_ID = "CHANGE_ID";
 
-const reducer = (state = { todos: [] }, action) => {
+export const getTodos = (id) => {
+  return async (dispatch) => {
+    let response = await fetch(
+      `https://jsonplaceholder.typicode.com/todos?userId=${id}`
+    );
+    response = await response.json();
+    if (response) {
+      dispatch({ type: GET_TODOS, payload: response });
+    }
+  };
+};
+
+export const changeId = (id) => {
+  if (id > 0 && id < 12) {
+    return { type: CHNG_ID, payload: id };
+  }
+};
+
+const reducer = (state = { todos: [], id: 1 }, action) => {
   switch (action.type) {
     case "GET_TODOS":
-      return state;
+      console.log("came here (before)");
+      // console.log("came here");
+      console.log(state);
+      return {
+        ...state,
+        todos: action.payload,
+      };
+    case "CHNG_ID":
+      return { ...state, id: action.payload };
     default:
       return state;
   }
 };
+
+export default reducer;
